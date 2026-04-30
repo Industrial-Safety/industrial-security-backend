7710441e994734bbff0e4a54c5be98ebb964f9a4
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-04-30 16:29:56 -0500

    changing the function of s3

--- course-service/src/main/java/com/industrial/safety/course_service/config/S3Config.java
+++ course-service/src/main/java/com/industrial/safety/course_service/config/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config
@@ -21,9 +22,9 @@ public class S3Config
     private String region;
 
     @Bean
-    public S3Client s3Client(){
-        AwsBasicCredentials  awsBasicCredentials=AwsBasicCredentials.create(secretKey, accesKey);
-        return S3Client.builder()
+    public S3Presigner s3Presigner(){
+        AwsBasicCredentials  awsBasicCredentials=AwsBasicCredentials.create(accesKey, secretKey);
+        return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
                 .build();
