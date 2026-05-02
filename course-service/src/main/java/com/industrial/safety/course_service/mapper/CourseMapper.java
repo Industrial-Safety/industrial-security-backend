378fcd835355956877e05744eed2d95df4c1d440
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-05-01 20:54:26 -0500

    todo

--- course-service/src/main/java/com/industrial/safety/course_service/mapper/CourseMapper.java
+++ course-service/src/main/java/com/industrial/safety/course_service/mapper/CourseMapper.java
@@ -6,6 +6,7 @@ import com.industrial.safety.course_service.model.component.Lecture;
 import com.industrial.safety.course_service.model.component.Resource;
 import com.industrial.safety.course_service.model.component.Section;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper
@@ -19,5 +20,5 @@ public interface CourseMapper
     ResourceResponse toResourceResponse(Resource resource);
     SectionResponse toSectionResponse(Section section);
 
-    void  updateCourseFromRqueset(CourseRequest courseRequest,Course course);
+    void  updateCourseFromRqueset(CourseRequest courseRequest,@MappingTarget Course course);
 }
