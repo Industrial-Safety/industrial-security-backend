516da1c97f0ed69ae7e47534c112b5b59d948a25
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-05-05 17:16:39 -0500

    feat: implementar microservicios y corregir configuración de gitignore

--- user-service/src/main/java/com/industrial/safety/user_service/dto/UserResponse.java
+++ user-service/src/main/java/com/industrial/safety/user_service/dto/UserResponse.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 @Builder
 public class UserResponse
 {
-    private Long id;
+    private String id;
     private String keycloakId;
     private String name;
     private String lastName;
