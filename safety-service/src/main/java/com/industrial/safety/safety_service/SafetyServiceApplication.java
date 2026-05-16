ed7b2119b1186522dab5fcfdcd3a80e5acb1a765
Im3Im3 <manuelcode737@gmail.com>
2026-05-16 10:35:02 -0500

    Add funcionality and replaced in docker compose

--- safety-service/src/main/java/com/industrial/safety/safety_service/SafetyServiceApplication.java
+++ safety-service/src/main/java/com/industrial/safety/safety_service/SafetyServiceApplication.java
@@ -2,6 +2,9 @@ package com.industrial.safety.safety_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SafetyServiceApplication {
@@ -10,4 +13,16 @@ public class SafetyServiceApplication {
 		SpringApplication.run(SafetyServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PATCH", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
