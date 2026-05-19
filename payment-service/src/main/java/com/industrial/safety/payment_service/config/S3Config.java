d8171ff217adc5046a9c00ff55c85acd338ba34b
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-05-19 00:17:26 -0500

    payment-service

--- payment-service/src/main/java/com/industrial/safety/payment_service/config/S3Config.java
+++ payment-service/src/main/java/com/industrial/safety/payment_service/config/S3Config.java
@@ -3,39 +3,31 @@ package com.industrial.safety.payment_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-public class S3Config {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
+public class S3Config
+{
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public S3Client s3Client() {
-        AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Client.builder()
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
     @Bean
-    public S3Presigner s3Presigner() {
-        AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey, secretKey);
-        return S3Presigner.builder()
+    public S3Client s3Client() {
+        return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
+
 }
