d9b205fe3c01e938ac8b3d42f23abb4fb545a0f8
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-06-06 13:58:37 -0500

    fix: borrar clases duplicadas en tests y corregir config de seguridad en api-gateway

--- purchase-service/src/test/java/com/logistica/purchase/repository/PurchaseRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.logistica.purchase.repository;
-
-import com.logistica.purchase.entity.PurchaseRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest, Long> {
-}
