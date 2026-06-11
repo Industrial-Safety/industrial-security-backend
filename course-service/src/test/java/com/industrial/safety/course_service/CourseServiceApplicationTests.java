17e739c5511a2432de1662f9c38c1ba4ed19df75
ricardoIsmael <ricardo.i.palomino@gmail.com>
2026-06-11 12:21:06 -0500

    fix(test): course-service contextLoads extiende BaseCourseIT y bloquea parameterstore

--- course-service/src/test/java/com/industrial/safety/course_service/CourseServiceApplicationTests.java
+++ course-service/src/test/java/com/industrial/safety/course_service/CourseServiceApplicationTests.java
@@ -1,14 +1,18 @@
 package com.industrial.safety.course_service;
 
+import com.industrial.safety.course_service.integration.BaseCourseIT;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.config.import=",
+        "spring.cloud.aws.parameterstore.enabled=false"
+})
 @ActiveProfiles("test")
 @Tag("integration")
-class CourseServiceApplicationTests {
+class CourseServiceApplicationTests extends BaseCourseIT {
 
     @Test
     void contextLoads() {
